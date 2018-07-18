890fea3c99d84cb2549535e6f7abc9f0182abae7
unknown <palmercampbell99@gmail.com>
2018-07-18 08:37:17 -0600

    All done (not black diamond)

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -7,16 +7,41 @@ import SearchIcon from 'react-icons/lib/md/search';
 //////////////////////////////////////////////////////// THIS COMPONENT IS BEING RENDERED IN THE *HEADER* COMPONENT
 
 export default class Search extends Component {
+constructor(){
+  super();
+
+  this.state={
+    userInput : ''
+  }
+}
+
+  handleChange = (event) =>{
+    console.log(event.target.value);
+    this.setState({
+      userInput : event.target.value
+    })
+  }
+  handleClick=()=>{
+    let searchPost = [];
+    for (let i = 0; i < this.props.post.length; i++) {
+      if(this.props.post[i].text.includes(this.state.userInput)){
+        searchPost.push(this.props.post[i])
+      }
+    }
+    console.log(searchPost);
+  }
+
   render() {
+    console.log('string', this.props.post);
     return (
       <section className="Search__parent">
 
         <div className="Search__content">
-          <input placeholder="Search Your Feed" />
+          <input placeholder="Search Your Feed" onChange={this.handleChange} />
 
-          <SearchIcon id="Search__icon" />
+          <SearchIcon id="Search__icon"  onClick={this.handleClick}/>
         </div>
-        
+
       </section>
     )
   }
